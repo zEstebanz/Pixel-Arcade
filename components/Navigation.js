57986a6b4463ae71a23e605e9e63d8e3de64785b
Esteban Oller <estebanoller73@gmial.com>
2023-05-06 16:26:55 -0300

    Modificaciones

--- components/Navigation.js
+++ components/Navigation.js
@@ -14,22 +14,22 @@ const Navigation = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
+                        <li className={`${estilos.btnPixelCustom3} nav-item`}>
                             <Link href="/"  className="nav-link">
                                 Home
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className={`${estilos.btnPixelCustom2} nav-item`}>
                             <Link href="/about" className="nav-link">
                                 About
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className={`${estilos.btnPixelCustom1} nav-item`}>
                             <Link href="/contact" className="nav-link">
                                 Contact
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className={`${estilos.btnPixelCustom3} nav-item`}>
                             <Link href="/game" className="nav-link">
                                 Games
                             </Link>
